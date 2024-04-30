f873148cd732ef6288231dcd3c6caa8f28d246e0
Arup Sarkar <arup.uadda@gmail.com>
2024-05-01 03:46:55 +0530

    loki

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const config = {
     port: 8080,
     jwtSecret: 'jytBTKqyY2AUH4S1JY460l4zAbtDbwTg',
     db: process.env.MYSQL || 'mysql://root:Aspire_5742@192.168.1.102/directory',
-    LOKI_URL: "http://192.168.1.102:3100"
+    LOKI_URL: process.env.LOKI || "http://192.168.1.102:3100"
 }
 
 module.exports = config
