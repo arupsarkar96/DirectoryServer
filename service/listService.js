e0b53a23cffff76f008428c545393ef5dc359f56
Arup Sarkar <arup.uadda@gmail.com>
2024-05-01 19:30:30 +0530

    list update

--- service/listService.js
+++ service/listService.js
@@ -2,15 +2,27 @@ const { getConnection } = require("../config/db")
 
 
 const getListService = async (identifier, limit, offset) => {
-    const connection = await getConnection()
 
-    const sql = 'SELECT COUNT(*) AS count FROM `List` WHERE `identifier` = ?; SELECT * FROM `List` WHERE `identifier` = ? ORDER BY `sequence` ASC LIMIT ? OFFSET ?;'
-    const value = [identifier, identifier, limit, offset]
 
-    const [result, fields] = await connection.query(sql, value)
-    connection.release()
-    return result
+    if (identifier == "PID") {
+        const connection = await getConnection()
 
+        const sql = 'SELECT COUNT(*) AS count FROM `List` WHERE `identifier` = ?; SELECT * FROM `List` WHERE `identifier` = ? ORDER BY `timestamp` DESC LIMIT ? OFFSET ?;'
+        const value = [identifier, identifier, limit, offset]
+
+        const [result, fields] = await connection.query(sql, value)
+        connection.release()
+        return result
+    } else {
+        const connection = await getConnection()
+
+        const sql = 'SELECT COUNT(*) AS count FROM `List` WHERE `identifier` = ?; SELECT * FROM `List` WHERE `identifier` = ? ORDER BY `sequence` ASC LIMIT ? OFFSET ?;'
+        const value = [identifier, identifier, limit, offset]
+
+        const [result, fields] = await connection.query(sql, value)
+        connection.release()
+        return result
+    }
 }
 
 const getClubListService = async (identifier, limit, offset) => {
